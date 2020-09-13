fce754aa58ca3c4a45ce9889c41dea3f0c7fa979
David Gagnon <toogreen@gmail.com>
2020-09-12 22:11:37 -0400

    added some code to try and save an array in a localStorage variable to save previous searches... but it doesn't work yet so i commented it for now. all this in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,34 @@ const App = () => {
             // console.log(DataView)
             setWeather(data);
             setQuery('');
+            // Added the below so that Android keyboard disappears when all is done.
             $(document.activeElement).filter(':input:focus').blur();
+
+
+            // Added the below so use localStorage to remember this search
+/*             if (typeof(Storage) !== "undefined") {
+
+
+                var addToLocalStorageObject = function (name, key, value) {
+
+                    // Get the existing data
+                    var existing = localStorage.getItem("newsearch") 
+                
+                    // If no existing data, create an array
+                    // Otherwise, convert the localStorage string to an array
+                    existing = existing ? JSON.parse(existing) : {};
+                
+                    // Add new data to localStorage Array
+                    existing[key] = query;
+                
+                    // Save back to localStorage
+                    localStorage.setItem("newsearch", JSON.stringify(existing));
+
+                
+                };
+            
+            } */
+
         }
     }
 
